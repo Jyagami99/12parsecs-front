c4457ee71c79ce178394dff59c3cc4c463fc09a2
Carla <carla.dayane.berredo@gmail.com>
2022-07-08 22:14:59 -0400

    chore: falta colocar o header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import { Home } from "./pages/Home";
+import SignUp from './pages/SignUp';
+import {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from '../Assets/GlobalStyles'
 
 function App() {
-  return <Home />;
+
+  const [user, setUser] = useState(
+    localStorage.getItem('userdata')
+    ? JSON.parse(localStorage.getItem('userdata'))
+    : null
+);
+
+
+  return(
+    
+    <BrowserRouter>
+      <GlobalStyles/>
+      <Routes>
+        <Route path='/sign-up' element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
