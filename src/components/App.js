23747b8f49e9576a2fe36b4b4262c773efbdcce6
Joao Marcelo <jm.castro@usp.br>
2022-07-11 04:25:19 -0300

    fix: some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,28 @@
-import SignUp from './pages/SignUp';
-import {useState} from 'react';
-import { Routes, Route } from 'react-router-dom';
+import SignUp from "./pages/SignUp";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyles from '../Assets/GlobalStyles';
-import  SignIn  from "./pages/SignIn";
+import GlobalStyles from "../assets/GlobalStyles";
+import SignIn from "./pages/SignIn";
 import UserContext from "./context/userContext";
 
 function App() {
-
   const [user, setUser] = useState(
-    localStorage.getItem('userdata')
-    ? JSON.parse(localStorage.getItem('userdata'))
-    : null
-);
-  return(
-    <UserContext.Provider value={{user, setUser}}>
+    localStorage.getItem("userdata")
+      ? JSON.parse(localStorage.getItem("userdata"))
+      : null
+  );
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
       <BrowserRouter>
-        <GlobalStyles/>
+        <GlobalStyles />
         <Routes>
-          <Route path='/sign-up' element={<SignUp />}/>
-          <Route path='/sign-in' element={<SignIn />}/>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
         </Routes>
-    </BrowserRouter>
-
+      </BrowserRouter>
     </UserContext.Provider>
-    
-  )
+  );
 }
 
 export default App;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,209 +1,222 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import dotenv from "dotenv";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
-import {Link, useNavigate} from 'react-router-dom';
-import styled from 'styled-components';
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 dotenv.config();
 
-function SignUp(){
+function SignUp() {
+  const [data, setdata] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmaPassword: "",
+    erro: false,
+  });
 
-    const [data, setdata] = useState({
-        name: '',
-        email: '',
-        password: '',
-        confirmaPassword: '',
-        erro: false
+  const [buttonSignUp, setbuttonSignUp] = useState({
+    desabilitado: true,
+    className: "botaoDesabilitado",
+  });
+
+  const [loadingData, setloadingData] = useState({
+    loading: false,
+    className: "",
+  });
+
+  const navigate = useNavigate();
+
+  const signUp = (e) => {
+    e.preventDefault();
+
+    setloadingData({
+      ...loadingData,
+      loading: true,
+      className: "inputDesabilitado",
     });
 
-    const [buttonSignUp, setbuttonSignUp] = useState({
-        desabilitado: true,
-        className: 'botaoDesabilitado'
-    });
+    const URL = `https://project12parsecs.herokuapp.com/sign-up`;
 
-    const [loadingData, setloadingData] = useState({
-        loading: false,
-        className: ''
-    });
-
-    const navigate = useNavigate();
-
-    const signUp = (e) => {
-        e.preventDefault();
-
+    axios
+      .post(URL, {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      })
+      .then((response) => {
+        console.log(response.data);
         setloadingData({
-            ...loadingData,
-            loading: true,
-            className: 'inputDesabilitado'
+          ...loadingData,
+          loading: false,
+          className: "",
         });
-
-        const URL = `https://project12parsecs.herokuapp.com/sign-up`;
-
-        axios.post(URL,{
-            name: data.name,
-            email: data.email,
-            password: data.password
-        }).then((response) => {
-            console.log(response.data);
-            setloadingData({
-                ...loadingData,
-                loading: false,
-                className: ''
-            });
-            navigate('/sign-in');
-        }).catch((erro) => {
-            console.log({message: 'Deu ruim', erro});
-            setloadingData({
-                ...loadingData,
-                loading: false,
-                className: ''
-            });
+        navigate("/sign-in");
+      })
+      .catch((erro) => {
+        console.log({ message: "Deu ruim", erro });
+        setloadingData({
+          ...loadingData,
+          loading: false,
+          className: "",
         });
-    };
+      });
+  };
 
-    useEffect(()=>{
-        if(data.confirmaPassword !== '' && data.password !== data.confirmaPassword){
-            setdata({...data, erro: true});
-            setbuttonSignUp({
-                ...buttonSignUp, 
-                desabilitado: true,
-                className: 'botaoDesabilitado'
-            });
-        }
-        if(data.confirmaPassword !== '' && data.password === data.confirmaPassword){
-            setdata({
-                ...data,
-                erro: false
-            });
-            setbuttonSignUp({
-                ...buttonSignUp,
-                desabilitado: false,
-                className: ''
-            });
-        }
-    }, [data.confirmaPassword, data.password]);
+  useEffect(() => {
+    if (
+      data.confirmaPassword !== "" &&
+      data.password !== data.confirmaPassword
+    ) {
+      setdata({ ...data, erro: true });
+      setbuttonSignUp({
+        ...buttonSignUp,
+        desabilitado: true,
+        className: "botaoDesabilitado",
+      });
+    }
+    if (
+      data.confirmaPassword !== "" &&
+      data.password === data.confirmaPassword
+    ) {
+      setdata({
+        ...data,
+        erro: false,
+      });
+      setbuttonSignUp({
+        ...buttonSignUp,
+        desabilitado: false,
+        className: "",
+      });
+    }
+  }, [data.confirmaPassword, data.password]);
 
-    return(
-        <>
-            <Topo>
-                <Link to="/">
-                    <ion-icon name="home-outline"></ion-icon>
-                </Link>
-                <h1>12 Parsecs</h1>
-                <Link to="/carrinho">
-                    <ion-icon name="cart-outline"></ion-icon>
-                </Link>  
-            </Topo>
-            <CadastroContainer>
-                <Form onSubmit={signUp}>
-                    <input
-                        type="text"
-                        disabled={loadingData.loading}
-                        className={loadingData.className}
-                        placeholder="Nome"
-                        required value={data.name}
-                        onChange={(e) => setdata({...data, name: e.target.value})}
-                    />
-                    <input
-                        type="email"
-                        disabled={loadingData.loading}
-                        className={loadingData.className}
-                        placeholder="E-mail"
-                        required value={data.email}
-                        onChange={(e) => setdata({...data, email: e.target.value})}
-                    />
-                    <input
-                        type="password"
-                        disabled={loadingData.loading}
-                        className={loadingData.className}
-                        placeholder="Senha"
-                        required value={data.password}
-                        onChange={(e) => setdata({...data, password: e.target.value})}
-                    />
-                    <input
-                        type="password"
-                        disabled={loadingData.loading}
-                        className={loadingData.className}
-                        placeholder="Confirme a senha"
-                        required value={data.confirmaPassword}
-                        onChange={(e) => setdata({...data, confirmaPassword: e.target.value})}
-                    />
+  return (
+    <>
+      <Topo>
+        <Link to="/">
+          <ion-icon name="home-outline"></ion-icon>
+        </Link>
+        <h1>12 Parsecs</h1>
+        <Link to="/carrinho">
+          <ion-icon name="cart-outline"></ion-icon>
+        </Link>
+      </Topo>
+      <CadastroContainer>
+        <Form onSubmit={signUp}>
+          <input
+            type="text"
+            disabled={loadingData.loading}
+            className={loadingData.className}
+            placeholder="Nome"
+            required
+            value={data.name}
+            onChange={(e) => setdata({ ...data, name: e.target.value })}
+          />
+          <input
+            type="email"
+            disabled={loadingData.loading}
+            className={loadingData.className}
+            placeholder="E-mail"
+            required
+            value={data.email}
+            onChange={(e) => setdata({ ...data, email: e.target.value })}
+          />
+          <input
+            type="password"
+            disabled={loadingData.loading}
+            className={loadingData.className}
+            placeholder="Senha"
+            required
+            value={data.password}
+            onChange={(e) => setdata({ ...data, password: e.target.value })}
+          />
+          <input
+            type="password"
+            disabled={loadingData.loading}
+            className={loadingData.className}
+            placeholder="Confirme a senha"
+            required
+            value={data.confirmaPassword}
+            onChange={(e) =>
+              setdata({ ...data, confirmaPassword: e.target.value })
+            }
+          />
 
-                    {data.erro ? (
-                        <span className="msg">As senhas não são iguais</span>
-                    ) : null}
+          {data.erro ? (
+            <span className="msg">As senhas não são iguais</span>
+          ) : null}
 
-                    {loadingData.loading === false ?  (
-                        <button type="submit" className={buttonSignUp.className} disabled={buttonSignUp.desabilitado}>
-                            Cadastrar
-                        </button>
-                    ) : (
-                        <button type="button" disabled>
-                            <ThreeDots
-                                color="rgba(255, 255, 255, 1)"
-                                height={13}
-                                width={51}
-                            />
-                        </button>
-                    )}
-                </Form>
-                <Link to="/sign-in">
-                    <p>já tem conta? Entre agora!</p>
-                </Link>
-            </CadastroContainer>
-        </>
-        
-    )
-
+          {loadingData.loading === false ? (
+            <button
+              type="submit"
+              className={buttonSignUp.className}
+              disabled={buttonSignUp.desabilitado}
+            >
+              Cadastrar
+            </button>
+          ) : (
+            <button type="button" disabled>
+              <ThreeDots
+                color="rgba(255, 255, 255, 1)"
+                height={13}
+                width={51}
+              />
+            </button>
+          )}
+        </Form>
+        <Link to="/sign-in">
+          <p>já tem conta? Entre agora!</p>
+        </Link>
+      </CadastroContainer>
+    </>
+  );
 }
 
-
-const CadastroContainer =  styled.div`
-
-    p {
-        color: #ffffff;
-        font-size: 15px;
-        font-weight: 700;
-        font-style: normal;
-        text-align: center;
-        margin-top: 32px;
-    }
+const CadastroContainer = styled.div`
+  p {
+    color: #ffffff;
+    font-size: 15px;
+    font-weight: 700;
+    font-style: normal;
+    text-align: center;
+    margin-top: 32px;
+  }
 `;
 
 const Topo = styled.header`
-   display: fixed;
-   height: 60px;
-   width: 100%;
-   margin: auto; 
-   display: flex;
-   justify-content: space-around;
-   align-items: center;
-   background-color: black;
+  display: fixed;
+  height: 60px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
 
-   ion-icon{
+  ion-icon {
     color: white;
     font-size: 30px;
-   }
-   
-   
-   h1{
-    font-family: 'Raleway';
+  }
+
+  h1 {
+    font-family: "Raleway";
     font-style: normal;
     font-weight: 700;
     font-size: 26px;
     line-height: 31px;
-    color: #FFFFFF;
-   }
- `;
+    color: #ffffff;
+  }
+`;
 
 const Form = styled.form`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin-top: 95px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 95px;
 
-input {
+  input {
     width: 326px;
     height: 58px;
     border-radius: 5px;
@@ -212,28 +225,28 @@ input {
     padding-left: 15px;
     margin-bottom: 13px;
     font-size: 20px;
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     &:focus {
-        outline: none;
+      outline: none;
     }
     &::placeholder {
-        font-style: regular;
-        font-weight: 400;
-        font-size: 20px;
-        color: #000000;
+      font-style: regular;
+      font-weight: 400;
+      font-size: 20px;
+      color: #000000;
     }
-}
+  }
 
-input:focus::placeholder {
+  input:focus::placeholder {
     color: transparent;
-}
+  }
 
-button {
+  button {
     width: 326px;
     height: 46px;
     background-color: rgb(163, 40, 214);
     color: #ffffff;
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     font-size: 20px;
     font-weight: 700;
     border-radius: 5px;
@@ -241,21 +254,21 @@ button {
     display: flex;
     align-items: center;
     justify-content: center;
-}
+  }
 
-.botaoDesabilitado {
+  .botaoDesabilitado {
     background-color: rgba(93, 93, 94, 0.5);
-}
+  }
 
-.inputDesabilitado {
+  .inputDesabilitado {
     background-color: rgba(212, 212, 212, 1);
     color: rgba(175, 175, 175, 1);
-}
+  }
 
-.msg {
+  .msg {
     color: #ffffff;
     margin-bottom: 13px;
-}
+  }
 `;
 
 export default SignUp;
